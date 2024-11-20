9bb01640c0b9da2311dfa1447cb7b3d617cff276
CynthiaMorin <cynthiamorin00@gmail.com>
2024-11-20 13:09:23 -0600

    Update test_television.py

--- test_television.py
+++ test_television.py
@@ -1,18 +1,18 @@
 import pytest
 from television import Television
 
-def test_initial_state():
+def test_initial_state(): """Tests the initial state of the televison"""
     tv = Television()
     assert str(tv) == "Power: Off, Channel: 0, Volume: 0"
 
-def test_power():
+def test_power(): """Tests that the power on the television has the ability to turn on and off"""
     tv = Television()
     tv.power()
     assert str(tv) == "Power: On, Channel: 0, Volume: 0"
     tv.power()
     assert str(tv) == "Power: Off, Channel: 0, Volume: 0"
 
-def test_mute():
+def test_mute(): """Tests the status of the mute feature at the beginning of the program"""
     tv = Television()
     tv.power()
     tv.volume_up()
@@ -21,7 +21,7 @@ def test_mute():
     tv.mute()
     assert str(tv) == "Power: On, Channel: 0, Volume: 1"
 
-def test_channel_up():
+def test_channel_up(): """Tests the ability of the tv channels to be increased by 1"""
     tv = Television()
     tv.power()
     tv.channel_up()
@@ -31,7 +31,7 @@ def test_channel_up():
     tv.channel_up()
     assert str(tv) == "Power: On, Channel: 0, Volume: 0"
 
-def test_channel_down():
+def test_channel_down(): """Tests the ability of the tv channels to be decreased by 1"""
     tv = Television()
     tv.power()
     tv.channel_down()
@@ -39,7 +39,7 @@ def test_channel_down():
     tv.channel_down()
     assert str(tv) == "Power: On, Channel: 2, Volume: 0"
 
-def test_volume_up():
+def test_volume_up(): """Tests the ability of the volume feature to increase by 1"""
     tv = Television()
     tv.power()
     tv.volume_up()
@@ -50,7 +50,7 @@ def test_volume_up():
     tv.volume_up()  # Exceeding max volume
     assert str(tv) == "Power: On, Channel: 0, Volume: 2"
 
-def test_volume_down():
+def test_volume_down(): """Tests that the volume down feature can decrease the volume by 1"""
     tv = Television()
     tv.power()
     tv.volume_down()  # Already at minimum
